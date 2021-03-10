7acfab45d41c507804612343716215ef21d1c223
thanh khiet <cs.thanhkhiet@gmail.com>
2021-03-10 16:03:50 +0700

    ok

--- module5-final/js.js
+++ module5-final/js.js
@@ -1,5 +1,27 @@
-var name = ["thanh", "cao", "khiet"];
-name.greeting = "hi";
-for(var i = 0; i < name.length; i++){
-    console.log(name[i]);
-}
+document.addEventListener("DOMContentLoaded", (event) =>{
+    function sayHello(event){
+        
+        
+        var input = document.getElementById("input").value;
+    
+        document
+            .getElementById("content")
+            .innerHTML = "<h2>Hello " + input + "</h2"; 
+    }
+    document
+        .querySelector("button")
+        .addEventListener("click", sayHello);
+
+
+    document
+        .querySelector("body")
+        .addEventListener("mousemove", (event) =>{
+            if (event.shiftKey === true) {
+                console.log(event.clientX);
+                console.log(event.clientY);
+            }
+        });
+
+
+
+})
